ee9a843ba5f1b780036d8c0c3e0a821ae93dc783
Chuck Harmston <chuck@chuckharmston.com>
2014-04-08 15:21:17 -0500

    Add module for choosing/caching featured apps for display

--- src/media/js/settings_local.js
+++ src/media/js/settings_local.js
@@ -2,6 +2,7 @@ define('settings_local', [], function() {
     // Override settings here!
     return {
         // No trailing slash, please./
-        apiURL: window.location.origin
+        apiURL: window.location.origin,
+        numberFeatured: 3
     };
 });
