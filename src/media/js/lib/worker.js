dea9a8fea371b8d6f1686db8dbe53866e0f0735c
Christopher Van <cvan@mozilla.com>
2014-04-10 01:44:48 -0700

    Revert "Revert "use content hashes everywhere for appcached assets (bug 993919)""
    This reverts commit b23d925da62319f1dc3652b18d26174af56b656a.

--- src/media/js/lib/worker.js
+++ src/media/js/lib/worker.js
@@ -7,8 +7,10 @@ var index;
 
 function index(data) {
   // TODO: Have uglify inline these scripts when minfied.
-  importScripts(data.min ? 'lunr.min.js' : 'lunr.js');
-  importScripts(data.min ? 'lunr.unicodeNormalizer.min.js' : 'lunr.unicodeNormalizer.js');
+  importScripts(data.min ? '../../../media/js/lib/lunr.min.js' :
+                           '../../../media/js/lib/lunr.js');
+  importScripts(data.min ? '../../../media/js/lib/lunr.unicodeNormalizer.min.js' :
+                           '../../../media/js/lib/lunr.unicodeNormalizer.js');
 
   log('Loaded lunr v' + lunr.version);
 
