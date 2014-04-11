ee39cef3ede235097612a93e81155c19936f590d
Mathieu Pillard <m@virgule.net>
2014-04-11 19:56:56 +0200

    Re-indent main.js properly

--- src/media/js/main.js
+++ src/media/js/main.js
@@ -1,90 +1,90 @@
 define('main', [], function () {
-if (!window.location.origin) {
-  // For IE lol
-  window.location.origin = window.location.protocol + '//' + window.location.host;
-}
-if (!window.performance) {
-      // Polyfill `performance.now` for Safari/PhantomJS WebKit.
-      // (And don't even bother with `Date.now` because IE.)
-      window.performance = {
-          now: function() {
-              return +new Date();
-          }
-      };
-}
-window.start_time = window.performance.now();
-
-var hash = (/.*\.hash_(.*)\./.exec(require('routes_api')['search.docs.preloaded']) || [])[1] || '';
-document.body.dataset.preloaded_hash = hash;
-if (hash) {
-  var storage = require('storage');
-  var previousHash = storage.setItem('preloadedHash');
-  if (previousHash === hash) {
-    // Flush cached apps that we don't need anymore.
-    console.log('Removing old docs from local cache');
-    storage.removeItem('docs:' + hash);
-  } else {
-    storage.setItem('preloadedHash', hash);
+  if (!window.location.origin) {
+    // For IE lol
+    window.location.origin = window.location.protocol + '//' + window.location.host;
   }
-}
+  if (!window.performance) {
+    // Polyfill `performance.now` for Safari/PhantomJS WebKit.
+    // (And don't even bother with `Date.now` because IE.)
+    window.performance = {
+      now: function() {
+        return +new Date();
+      }
+    };
+  }
+  window.start_time = window.performance.now();
 
-var app = new window.routes();
-var GET = require('utils').parseQueryString();
+  var hash = (/.*\.hash_(.*)\./.exec(require('routes_api')['search.docs.preloaded']) || [])[1] || '';
+  document.body.dataset.preloaded_hash = hash;
+  if (hash) {
+    var storage = require('storage');
+    var previousHash = storage.setItem('preloadedHash');
+    if (previousHash === hash) {
+      // Flush cached apps that we don't need anymore.
+      console.log('Removing old docs from local cache');
+      storage.removeItem('docs:' + hash);
+    } else {
+      storage.setItem('preloadedHash', hash);
+    }
+  }
 
-if (GET.debug) {
-  window.location.href = 'debug.html';
-}
+  var app = new window.routes();
+  var GET = require('utils').parseQueryString();
 
-if (GET.lang) {
-  document.webL10n.setLanguage(GET.lang);
-}
+  if (GET.debug) {
+    window.location.href = 'debug.html';
+  }
 
-document.webL10n.ready(function() {
-  document.documentElement.dir = document.webL10n.getDirection();
-  document.documentElement.lang = document.webL10n.getLanguage();
+  if (GET.lang) {
+    document.webL10n.setLanguage(GET.lang);
+  }
 
-  var $ = require('dom');
-  var pages = require('pages');
-  var templating = require('templating');
+  document.webL10n.ready(function() {
+    document.documentElement.dir = document.webL10n.getDirection();
+    document.documentElement.lang = document.webL10n.getLanguage();
 
-  var views = {
-    '/app/:slug/': 'detail',
-    '/': 'search',
-  };
+    var $ = require('dom');
+    var pages = require('pages');
+    var templating = require('templating');
 
-  Object.keys(views).forEach(function(path) {
-    var view = require('views/' + views[path]);
-    app.get(path, function(req) {
-      document.title = pages.getTitle(req.url);
-      view.init(req.params);
+    var views = {
+      '/app/:slug/': 'detail',
+      '/': 'search',
+    };
+
+    Object.keys(views).forEach(function(path) {
+      var view = require('views/' + views[path]);
+      app.get(path, function(req) {
+        document.title = pages.getTitle(req.url);
+        view.init(req.params);
+      });
+    });
+
+    app.load = function(url) {
+      window.history.pushState(undefined, pages.getTitle(url), url);
+
+      // We must have the exact pathname (no querystring parameters, etc.).
+      app.test(pages.getPath(url));
+    };
+
+    $.delegate('click', 'a[href^="/"]', function(e) {
+      e.preventDefault();
+      app.load(e.delegateTarget.getAttribute('href'));
+    });
+
+    $.delegate('keypress', 'body:not(.results) input[name=q]', function() {
+      app.load('/');
+    });
+
+    templating.render('header', function(res) {
+      $('.header').innerHTML = res;
+
+      // Because `routes.js` listens for `window.load` event, explicitly load view.
+      app.load(document.location.href);
     });
   });
 
-  app.load = function(url) {
-    window.history.pushState(undefined, pages.getTitle(url), url);
-
-    // We must have the exact pathname (no querystring parameters, etc.).
-    app.test(pages.getPath(url));
-  };
-
-  $.delegate('click', 'a[href^="/"]', function(e) {
-    e.preventDefault();
-    app.load(e.delegateTarget.getAttribute('href'));
-  });
-
-  $.delegate('keypress', 'body:not(.results) input[name=q]', function() {
-    app.load('/');
-  });
-
-  templating.render('header', function(res) {
-    $('.header').innerHTML = res;
-
-    // Because `routes.js` listens for `window.load` event, explicitly load view.
-    app.load(document.location.href);
-  });
-});
-
-return {app: app};
+  return {app: app};
 });
 
 require('main');
